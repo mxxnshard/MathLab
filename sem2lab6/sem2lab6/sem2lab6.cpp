0172c3b86fe8d149aed19d57951f0ebe6885df83
mxxnshard <81923336+mxxnshard@users.noreply.github.com>
2024-07-15 13:32:56 +0500

    Add files via upload

---
+++ sem2lab6/sem2lab6/sem2lab6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    bool f = true;
+
+    for (int i = 0; i <= n; ++i) {
+        string s;
+        cin >> s;
+        int k0 = 0, k1 = 0, k2 = 0;
+        for (int j = 0; j < s.size(); ++j) {
+            if (s[j] > s[j + 1] and i != s.size() - 1) {
+                f = false;
+            }
+            else if (s[i] == '0') {
+                k0++;
+            }
+            else if (s[i] == '1') {
+                k1++;
+            }
+            else if (s[i] == '2') {
+                k2++;
+            }
+        }
+        if (k1 == k0 and k0 == k2 and f) cout << "YES";
+        else cout << "NO";
+    }
+}
